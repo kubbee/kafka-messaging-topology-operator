a8cfa81a9f218e5fe2691a08fa89d850597f931d
lhsribas <lhs.ribas@gmail.com>
2022-09-19 16:54:28 -0300

    add apis KafkaConsumer, KafkaProcucer, KafkaACL

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	messagesv1beta1 "github.com/kubbee/kafka-messaging-topology-operator/api/v1beta1"
+	"github.com/kubbee/kafka-messaging-topology-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(messagesv1beta1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,27 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.KafkaConsumerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "KafkaConsumer")
+		os.Exit(1)
+	}
+	if err = (&controllers.KafkaProducerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "KafkaProducer")
+		os.Exit(1)
+	}
+	if err = (&controllers.KafkaACLReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "KafkaACL")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
